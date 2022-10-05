8843d61041de33ad2c2d9a28996d12ae92a6f09c
poncew <112092491+poncew@users.noreply.github.com>
2022-10-05 08:01:00 -0600

    Delete Lab_05 directory

--- Lab_05/Ejercicio1.py
+++
@@ -1,7 +0,0 @@
-try:
- entrada = input("Ingrese el nombre del archivo: ")
- archivo = open(entrada, "r", encoding="UTF-8")
- for Línea in archivo:
-  print(Línea.upper())
-except:
-    print("Error, archivo no existe")
